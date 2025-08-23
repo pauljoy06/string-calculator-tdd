07ac8fcc2974403e841659f858a51bf4eb1c414d
Paul Padamadan <paul.padamadan@programming.com>
2025-08-23 16:25:03 +0530

    RED: Add test for single number returns that number

--- test_string_calculator.py
+++ test_string_calculator.py
@@ -5,3 +5,8 @@ from string_calculator import add
 def test_empty_string_returns_zero():
     """Test that empty string returns 0"""
     assert add("") == 0
+
+
+def test_single_number_returns_that_number():
+    """Test that single number returns that number"""
+    assert add("1") == 1
