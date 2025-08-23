86272e9a9ce621afaf2d98d81d1d1e2b0242fdda
Paul Padamadan <paul.padamadan@programming.com>
2025-08-23 16:35:05 +0530

    GREEN: Make newline separator test pass

--- string_calculator.py
+++ string_calculator.py
@@ -2,5 +2,6 @@ def add(numbers: str) -> int:
     """String calculator that adds numbers from a string."""
     if numbers == "":
         return 0
+    numbers = numbers.replace("\n", ",")
     parts = numbers.split(",")
     return sum(int(part) for part in parts)
