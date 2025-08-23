7b205e78611b0d562c93cdad0d01e0aad19b191a
Paul Padamadan <paul.padamadan@programming.com>
2025-08-23 16:39:34 +0530

    RED: Add test for custom delimiters

--- test_string_calculator.py
+++ test_string_calculator.py
@@ -25,3 +25,8 @@ def test_multiple_comma_separated_numbers_returns_sum():
 def test_newline_as_separator():
     """Test that newlines can be used as separators"""
     assert add("1\n2,3") == 6
+
+
+def test_custom_delimiter():
+    """Test custom delimiter format: //[delimiter]\n[numbers...]"""
+    assert add("//;\n1;2") == 3
